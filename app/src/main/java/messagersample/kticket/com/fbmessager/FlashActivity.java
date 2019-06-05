d2f5f3b0fa5426a4e0c63f363117860c1ae39613
penhsokra <penhsokra@gmail.com>
2019-06-05 17:47:46 +0700

    flash screen add

---
+++ app/src/main/java/messagersample/kticket/com/fbmessager/FlashActivity.java
@@ -0,0 +1,13 @@
+package messagersample.kticket.com.fbmessager;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FlashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_flash);
+    }
+}
